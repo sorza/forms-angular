fe87600588b90cff66eca9e679891a3e1d406865
Alexandre Sorza <44484412+sorza@users.noreply.github.com>
2023-11-05 15:35:41 -0300

    Criando uma diretiva de validação e aplicando para data de nascimento

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,27 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: '[maiorIdadeValidator]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: MaiorIdadeDirective,
+    multi:true
+  }]
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validator{
 
   constructor() { }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoNascmais18 = anoNascimento + 18;
+    const anoAtual = new Date().getFullYear();
+
+    const ehMaior = anoNascmais18 <= anoAtual;
+
+    return ehMaior ? null : {'maiorIdadeValidator': true}
+  }
 
 }
